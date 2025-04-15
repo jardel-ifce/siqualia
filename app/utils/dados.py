acd1575a23fe02bbfe29ebfb3da191f374b940cb
reginaldo-pfr <reginaldo.fernands@gmail.com>
2025-04-15 03:43:52 -0300

    Atualização da forma de carregamento dinâmico dos arquivos json

--- app/utils/dados.py
+++ app/utils/dados.py
@@ -2,17 +2,21 @@ import json
 from pathlib import Path
 import unicodedata
 
-DATA_PATH = Path(__file__).parent.parent / "data" / "etapas_completas_com_medidas.json"
-
-def carregar_documentos():
-    try:
-        with open(DATA_PATH, "r", encoding="utf-8") as f:
-            return json.load(f)
-    except Exception as e:
-        raise RuntimeError(f"Erro ao carregar dados: {e}")
+DATA_DIR = Path(__file__).parent.parent / "data"
 
 def normalizar(texto: str):
     return unicodedata.normalize("NFKD", texto).encode("ASCII", "ignore").decode("ASCII").strip().lower()
+
+def carregar_documentos(produto: str):
+    try:
+        nome_arquivo = f"{normalizar(produto)}.json"
+        caminho = DATA_DIR / nome_arquivo
+        with open(caminho, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        raise RuntimeError(f"Arquivo de dados para o produto '{produto}' não encontrado.")
+    except Exception as e:
+        raise RuntimeError(f"Erro ao carregar dados: {e}")
 
 def buscar_etapa_por_nome(etapa_nome: str, documentos):
     etapa_norm = normalizar(etapa_nome)
